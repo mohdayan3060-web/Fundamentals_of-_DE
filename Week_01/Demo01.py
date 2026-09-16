d5f402f4227c54b654dcf9fbed51e55a437cd1e1
mohdayan0306-web <mohdayan0306@gmail.com>
2026-09-17 00:05:32 +0530

    Building Basic understanding of python syntex and also exploring python list

---
+++ Week_01/Demo01.py
@@ -0,0 +1,64 @@
+# Python Calculator 
+# Operator =input("Enter Operator (+  -  *  /)")
+# num1=float(input("Enter First Number : "))
+# num2=float(input("Enter Second Number : "))
+
+# if Operator== "+":
+#     result=num1+num2
+#     print("Result : " , result)
+# elif Operator == "-":
+#     result=num1-num2
+#     print("Result : " ,  result)
+# elif Operator == "*":
+#     result=num1*num2
+#     print("Result : " , result)
+# elif Operator == "/":
+#     result=num1/num2
+#     print("Result : " , result)
+# else:
+#     print( f"{Operator} is not valid Operators ")
+
+
+#using switch case 
+keepRuning=True
+while(keepRuning):
+    
+    print("1. Addition  ")
+    print("2. Subtraction ")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Integer Division")
+    print("6. Exit")
+    choice=int(input("Choose your Option "))
+    x=float(input("Enter First Number :  "))
+    y=float(input("Enter Second Number :  "))
+    match choice:
+        case 1:
+            result= x+y
+            print("Result : ", result)
+           
+        case 2:
+            result =x-y
+            print("Result : ", result)
+            
+
+        case 3 :
+            result =x*y
+            print("Result : ", result)
+            
+        case 4:
+            result= x/y
+            print("Result : ", result)
+            
+        case 5:
+            result=x//y 
+            print("Result : ",result )
+            
+        case 6:
+            keepRuning=False    
+            
+        case _:
+            print(" Please choose valid option ") 
+             
+
+    
